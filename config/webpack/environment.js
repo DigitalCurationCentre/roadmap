ecd4b04b401bde097cdb24e2d99cb8668977c07a
Bodacious <gavin@katanacode.com>
2018-09-28 16:58:28 +0100

    Fix webpack babel conversion

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
 const { environment } = require('@rails/webpacker')
 
-module.exports = environment
+const eslint = require('./loaders/eslint')
+const webpack = require('webpack')
+
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  jQuery: 'jquery',
+  $: 'jquery',
+  timeago: 'timeago.js',
+  Popper: ['popper.js', 'default'],
+}));
+
+environment.loaders.prepend('ESLint', eslint);
+
+module.exports = environment;
